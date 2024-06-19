8759913388b21003b6a9a1ed000015df28e4ce94
Estrella <165815792+Estre05@users.noreply.github.com>
2024-06-19 17:31:44 -0500

    commit

---
+++ 33-concatenar.js
@@ -0,0 +1,28 @@
+"Use strict";
+
+function elements(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            let resultado = nums[i] + '' + nums[j];
+            alert(resultado);
+        }
+    }
+}
+let nums = [0, 2, 4, 8];
+elements(nums);
+
+
+function elements(nums1) {
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = i + 1; j < nums1.length; j++) {
+            let resultado = nums1[i] + '' + nums1[j];
+            alert(resultado);
+        }
+    }
+}
+
+let nums1 = [0, 2, 4, 8];
+elements(nums1);
+
+
+
